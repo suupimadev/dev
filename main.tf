021d99d0ea2a38bea4a0847390f1c2d9aedec9f5
suupimadev <suupima.tenziku@gmail.com>
2026-03-08 17:59:26 +0900

    TEST

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
 provider "aws" {
   region = "ap-northeast-1"
 }
-resource "aws_s3_bucket" "test" {
-  bucket = "your-unique-bucket-name-masamsa20260117"
+
+resource "aws_instance" "demo" {
+  ami           = "ami-070d2b24928913a49" # Amazon Linux 2 (東京)
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform-ec2-test"
+  }
 }
-# feature branch test
