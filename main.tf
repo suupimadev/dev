0d20307be80cf4f37c1295f88499b691ec0c5c25
suupimadev <suupima.tenziku@gmail.com>
2026-03-08 18:19:57 +0900

    TEST-SG

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
-provider "aws" {
-  region = "ap-northeast-1"
-}
+resource "aws_security_group" "web_sg" {
+  name = "terraform-web-sg"
 
-resource "aws_instance" "demo" {
-  ami           = "ami-070d2b24928913a49" # Amazon Linux 2 (東京)
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "terraform-ec2-test"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
