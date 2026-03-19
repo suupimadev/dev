c3c97bdcc3fc55042f17fecadd150a88d3302cb4
suupimadev <suupima.tenziku@gmail.com>
2026-03-19 15:52:56 +0900

    SG-EC2

--- main.tf
+++ main.tf
@@ -8,3 +8,15 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+resource "aws_instance" "web" {
+  ami           = "ami-088b486f20fab3f0e"
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids = [
+    aws_security_group.web_sg.id
+  ]
+
+  tags = {
+    Name = "terraform-web"
+  }
+}
